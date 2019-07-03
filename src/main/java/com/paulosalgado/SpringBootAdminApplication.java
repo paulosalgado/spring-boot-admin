8d312a6b66ca3d3623bfb51b857ed6233caeb8f1
Paulo José de Oliveira Salgado <paulo@technosoftware.com.br>
2019-07-03 13:39:36 -0300

    Incluindo configurações de integração com o Consul.

--- src/main/java/com/paulosalgado/SpringBootAdminApplication.java
+++ src/main/java/com/paulosalgado/SpringBootAdminApplication.java
@@ -1,12 +1,13 @@
 package com.paulosalgado;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootAdminApplication {
 	
 	public static void main(String[] args) {
